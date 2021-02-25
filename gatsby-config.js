f0f4aa4e579148d748a484f8c54fe65ed7f811fa
Mykhailo Glubokyi <mee.six@gmail.com>
2021-02-25 21:26:56 +0200

    Google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,25 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-fontawesome-css`,
-
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-68777754-4",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
